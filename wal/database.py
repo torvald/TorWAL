4299293c5a6da02ff62bc1843dea08b9847278bc
Torvald Lekvam <117844+torvald@users.noreply.github.com>
2021-05-15 12:16:40 +0200

    Add the actual file

---
+++ wal/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+def setup_sqlite(database_file):
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
+    cursor.execute(
+        """CREATE TABLE if not exists x_log(
+        id INTEGER PRIMARY KEY,
+        active_win TEXT,
+        category TEXT,
+        idle INTEGER,
+        timestamp DATETIME DEFAULT (datetime('now','localtime'))
+    );"""
+    )
+    return connection
+
+
+def run_migrations(database_file):
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("ALTER TABLE x_log ADD COLUMN active_app TEXT default null")
+    except sqlite3.OperationalError as e:
+        assert "duplicate column name: active_app" in str(e)
